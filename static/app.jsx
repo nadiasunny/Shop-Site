9f213b099905b385d00a1d6ff79e4012d304911d
Nadia Hina <nadianadia062@gmail.com>
2024-05-01 22:47:17 -0700

    organized files

---
+++ static/app.jsx
@@ -0,0 +1,14 @@
+function MyNavbar() {
+    return (
+        <t>I am nav bar</t>
+    );
+}
+
+export default function MyApp() {
+    return (
+        <div>
+            <h>Testing</h>
+            <MyNavbar />
+        </div>
+    );
+}
